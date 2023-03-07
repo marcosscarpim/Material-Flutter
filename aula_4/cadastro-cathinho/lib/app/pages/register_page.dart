e67e1d538681f251defbe4c05a3d7616daa67d05
João Pedro <jpedrodaibello1999@outlook.com>
2023-03-07 15:59:17 -0300

    Criando botões - Tarefa Aula 4

--- aula_4/cadastro-cathinho/lib/app/pages/register_page.dart
+++ aula_4/cadastro-cathinho/lib/app/pages/register_page.dart
@@ -1,3 +1,7 @@
+import 'package:cadastro_cathinho/app/core/ui/styles/app_button_styles.dart';
+import 'package:cadastro_cathinho/app/core/ui/styles/app_colors.dart';
+import 'package:cadastro_cathinho/app/core/ui/styles/app_text_styles.dart';
+import 'package:cadastro_cathinho/app/core/ui/widgets/app_button.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatelessWidget {
@@ -10,7 +14,31 @@ class RegisterPage extends StatelessWidget {
         title: Image.asset('assets/images/logo.png'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            AppButton(
+              onPressed: () {},
+              style: context.buttonStyles.primaryOutlineButton,
+              height: 40,
+              width: MediaQuery.of(context).size.width,
+              label: 'Cancelar',
+              labelStyle: context.textStyles.textButtonLarge.copyWith(color: context.colors.secondary),
+              outline: true,
+            ),
+            const SizedBox(height: 24),
+            AppButton.primary(
+              onPressed: () {},
+              height: 40,
+              width: MediaQuery.of(context).size.width,
+              label: 'Cadastrar',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
